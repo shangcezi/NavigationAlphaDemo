401a307ae588aa7a63e68bb83c66500ff203c01b
1609865258@qq.com <xiaobu09>
2020-08-21 09:08:33 +0800

    4444

---
+++ NavigationAlphaDemo/CopyQQNav/OldObject.h
@@ -0,0 +1,17 @@
+//
+//  OldObject.h
+//  NavigationAlphaDemo
+//
+//  Created by apple on 2020/8/21.
+//  Copyright © 2020 PersonLiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OldObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NavigationAlphaDemo/CopyQQNav/OldObject.m
@@ -0,0 +1,13 @@
+//
+//  OldObject.m
+//  NavigationAlphaDemo
+//
+//  Created by apple on 2020/8/21.
+//  Copyright © 2020 PersonLiu. All rights reserved.
+//
+
+#import "OldObject.h"
+
+@implementation OldObject
+
+@end
